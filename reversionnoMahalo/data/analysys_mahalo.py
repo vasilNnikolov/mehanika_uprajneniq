a4d342d081bfcff16b04d28f80c711ca20becb62
vasil <vaskonikolov2003@gmail.com>
2022-01-03 23:47:03 +0200

    unfinished reversionno mahalo

--- reversionnoMahalo/data/analysys_mahalo.py
+++ reversionnoMahalo/data/analysys_mahalo.py
@@ -7,7 +7,11 @@ df.drop(df.columns[3:], axis=1, inplace=True)
 
 plt.plot(df["l, cm"], df["T1, s"])
 plt.plot(df["l, cm"], df["T2, s"])
+
+plt.plot(47.05, 1.3643, "ro")
+
+plt.xlabel("Дължина между тежестите, см")
+plt.ylabel("Период, с")
+
 plt.show()
 
-
-print(df)
