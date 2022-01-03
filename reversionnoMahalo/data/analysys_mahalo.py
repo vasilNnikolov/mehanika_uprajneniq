13c7b03505ac9fe180147f68f445bf63ae6f26f4
vasil <vaskonikolov2003@gmail.com>
2022-01-03 23:15:20 +0200

    added analysis file from old commit

---
+++ reversionnoMahalo/data/analysys_mahalo.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("./mahaloData.csv")
+
+df.drop(df.columns[3:], axis=1, inplace=True)
+
+plt.plot(df["l, cm"], df["T1, s"])
+plt.plot(df["l, cm"], df["T2, s"])
+plt.show()
+
+
+print(df)
