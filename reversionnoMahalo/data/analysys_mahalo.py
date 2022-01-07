0f0987a30bfa8f9f01ca2bcc7d7c90e715495371
vasil <vaskonikolov2003@gmail.com>
2022-01-07 13:26:51 +0200

    finished reversionno mahalo

--- reversionnoMahalo/data/analysys_mahalo.py
+++ reversionnoMahalo/data/analysys_mahalo.py
@@ -1,17 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import uncertainties
+import math
+from uncertainties.core import ufloat
 
-df = pd.read_csv("./mahaloData.csv")
+def main():
+    df = pd.read_csv("./mahaloData.csv")
 
-df.drop(df.columns[3:], axis=1, inplace=True)
+    df.drop(df.columns[3:], axis=1, inplace=True)
 
-plt.plot(df["l, cm"], df["T1, s"])
-plt.plot(df["l, cm"], df["T2, s"])
+    plt.plot(df["l, cm"], df["T1, s"])
+    plt.plot(df["l, cm"], df["T2, s"])
 
-plt.plot(47.05, 1.3643, "ro")
+    plt.plot(47.05, 1.3643, "ro")
 
-plt.xlabel("Дължина между тежестите, см")
-plt.ylabel("Период, с")
+    plt.xlabel("Дължина между тежестите, см")
+    plt.ylabel("Период, с")
 
-plt.show()
+    plt.show()
+
+def get_uncertainty():
+    T = ufloat(1.3653, 0.001)
+    l = ufloat(46.314, 0.002)
+    l = l/100
+    print(l)
+    g = 4*(math.pi)**2*l/T**2
+    print(g)
+
+if __name__ == "__main__":
+    get_uncertainty()
 
