214366fded31b3693edaf37031810892f8b14b97
vasil <vaskonikolov2003@gmail.com>
2022-05-03 17:54:57 +0300

    sync changes

--- 2_semestur/toploprovodnost/data/data_anal.py
+++ 2_semestur/toploprovodnost/data/data_anal.py
@@ -9,7 +9,16 @@ stationary = pd.read_csv(stationary_filepath)
 stationary = stationary[stationary.columns[:3]]
 
 # graph showing stationary state has been reached
-plt.figure(figsize=(16, 6))
-sns.scatterplot(data=stationary, x='t, min', y='T_heat, C', color='red')
-sns.scatterplot(data=stationary, x='t, min', y='T_ohlad, C', color='blue')
+# plt.figure(figsize=(16, 6))
+ax = sns.scatterplot(data=stationary, x='t, min', y='T_heat, C', color='red')
+sns.scatterplot(ax=ax, data=stationary, x='t, min', y='T_ohlad, C', color='blue')
+ax.axhline(55, color='red')
+ax.axhline(40.4, color='blue')
+
+ax.set_xlabel("t, min")
+ax.set_ylabel("T, C")
+
+ax.set_title("Достигане на стационарно състояние")
+
+ax.get_figure().savefig("graph_statz.png")
 plt.show()
