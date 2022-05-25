740382398de581c725d91c385e95939cc0077423
vasil <vaskonikolov2003@gmail.com>
2022-05-25 21:03:04 +0300

    finish data analysis script

--- 2_semestur/surface_tension/data/data_anal.py
+++ 2_semestur/surface_tension/data/data_anal.py
@@ -9,16 +9,15 @@ print(type(data))
 
 data = data.iloc[1:10, :7].astype(float)
 
-print(data)
 rho = ufloat(997, 0.1)
 g = ufloat(9.81, 0.01)
 R = ufloat(0.563, 0.001) * 1/2000 
 col_lengths = data.notnull().sum()
 
-print(data.dtypes)
 average = {key: data[key].mean()/1000 for key in data.columns}
 print(average)
-errors = {key: (1 + (data[key].std())**2/col_lengths[key])**0.5  for key in data.columns}
+errors = {key: (1/1000**2 + (data[key].std()/1000)**2/col_lengths[key])**0.5  for key in data.columns}
 coeffs = {key: rho*g*R*ufloat(average[key], errors[key])/2 for key in data.columns}
 
-print(coeffs)
+for col in data.columns:
+    print(f"{col}: sf coeff is {coeffs[col].nominal_value} pm {coeffs[col].std_dev/coeffs[col].nominal_value*100:.2f}")
