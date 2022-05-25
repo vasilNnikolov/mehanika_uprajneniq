921867352f5944bbf0e8075911758869ee5c7f0a
vasil <vaskonikolov2003@gmail.com>
2022-05-25 22:44:32 +0300

    update files

--- 2_semestur/surface_tension/data/data_anal.py
+++ 2_semestur/surface_tension/data/data_anal.py
@@ -15,9 +15,11 @@ R = ufloat(0.563, 0.001) * 1/2000
 col_lengths = data.notnull().sum()
 
 average = {key: data[key].mean()/1000 for key in data.columns}
-print(average)
 errors = {key: (1/1000**2 + (data[key].std()/1000)**2/col_lengths[key])**0.5  for key in data.columns}
 coeffs = {key: rho*g*R*ufloat(average[key], errors[key])/2 for key in data.columns}
 
+B = 72.75/ufloat(average["h2o dest"], errors["h2o dest"])
+
+print(B, 100*B.std_dev/B.nominal_value)
 for col in data.columns:
     print(f"{col}: sf coeff is {coeffs[col].nominal_value} pm {coeffs[col].std_dev/coeffs[col].nominal_value*100:.2f}")
